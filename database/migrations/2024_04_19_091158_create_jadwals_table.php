67c9c8040b34932a32cc81b603809a2fae408b76
CandhyFadhila <omaplocandhy@gmail.com>
2024-05-30 06:59:24 +0700

    UPDATE: Column jadwals migration.

--- database/migrations/2024_04_19_091158_create_jadwals_table.php
+++ database/migrations/2024_04_19_091158_create_jadwals_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('jadwals', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
-            $table->date('tanggal');
+            $table->date('tanggal_mulai');
+            $table->date('tanggal_selesai');
             $table->foreignId('shift_id');
             $table->timestamps();
 
